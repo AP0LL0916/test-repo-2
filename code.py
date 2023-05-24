fe5838fc5e60c3ea3dc2dbc6f317fba6a54a9783
AP0LL0916 <116090905+AP0LL0916@users.noreply.github.com>
2023-05-24 10:41:53 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,19 @@
+
+f = input('your fist exam: ')
+s = input('your second exam: ')
+t = input('your third exam: ')
+account_password = 'wwhgwywtwytssfgsfdgg267263ajgshjs'
+un = 'ahjhjs4jhjdh@gmail.com'
+average =(float(f) +float(s)+float(t))/3
+print ("average :{0}".format(average))
+
+count = 0
+
+my_string = "Programiz"
+my_char = "r"
+
+for i in my_string:
+	if i == my_char:
+    	count += 1
+print("Your word is " +my_string)
+print("Total number of occurence of char " +my_char +" is "+ str(count))
